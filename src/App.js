a3bb27f2f32bef6e16db574aa84d44e9e73f85a7
covivi <dan@dans-MacBook-Air.local>
2020-03-19 13:49:41 -0700

    with working version of satic map

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+
+import Map from  './component/Map'
+//import SearchableMap from './components/SearchableMap';
+import { Route, Switch, BrowserRouter } from 'react-router-dom'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-            Cure is coming
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <BrowserRouter >
+                <Switch>
+                    <Route exact path="/map" component={Map} />
+                </Switch>
+            </BrowserRouter>
+        </div>
+    )
 }
-
-export default App;
+export default App
